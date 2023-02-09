51a341c13daada4285d254f2e78cd1d3fb402257
Jesica Viola <111540797+jesica-viola@users.noreply.github.com>
2023-02-08 21:43:00 -0300

    java 2022
    UTN

---
+++ ProyectoCaja/ProyectoCaja/src/caja/PruebaCaja.java
@@ -0,0 +1,8 @@
+//proyecto caja: ejercicio1-> crear un proyecto segun las especificaciones
+//mostradas -> formula: volumen = ancho * alto * profundidad
+package caja;
+
+
+public class PruebaCaja {
+    
+}
